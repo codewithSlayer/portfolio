c723573e1c73527f94d17621055113e14b1964cf
Gaurav <123110682+codewithSlayer@users.noreply.github.com>
2023-12-10 11:16:18 +0530

    Add files via upload

---
+++ My_portFolioWebsite/index.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.typing-effect',{
+    strings:["Web Developer","Data Analysist","Web Designer","Data Scientist"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
